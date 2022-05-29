723aeb9530e81ee016ce7e7d6263c3420a8e48c5
DanielAvilez1 <104475313+DanielAvilez1@users.noreply.github.com>
2022-05-29 09:09:23 -0700

    homework

--- scripts/practice.js
+++ scripts/practice.js
@@ -19,30 +19,43 @@ let student = {
 document.write(student.address.city,student.address.country,student.address.street,);
 //console.log(student.hobbies)
 
-let product1={
-    name:"Laptop",
-    description:"macbook 16`",
-    price:1000,
+// let product1 ={
+//     name:"Laptop",
+//     description:"macbook 16`",
+//     price:1000,
+// }
+// let product2 ={
+//     name:"Monitor",
+//     description:"dell 27`",
+//     price:240,
+// }
+// let product3 ={
+//     name:"desk",
+//     description:"l shape",
+//     price:200,
+// }
+//let products = [];
+
+function Product(n,d,p){
+    this.name=n;
+    this.description=d;
+    this.price=p;
+
 }
-let product2={
-    name:"Monitor",
-    description:"dell 27`",
-    price:240,
-}
-let product3={
-    name:"desk",
-    description:"l shape",
-    price:200,
-}
-products =[product1,product2,product3];
+let product1 = new Product("Speaker","Sony Wireless",20.50);
+let product2 = new Product("Laptop","HP",100.00);
+let product3 = new Product("Monitor","AOC",350.00);
+let product4 = new Product("Phone","Apple",1199);
+//products =[product1,product2,product3];
+
+console.log(product1,product2,product3,product4);
 
 //products.push(product1,product2,product3);
 
 function displayName(){
     for(let i=0;i<products.length;i++){
-        
         console.log(products[i].description);
     }
 }
 
-displayName();
+//displayName();
--- scripts/register.js
+++ scripts/register.js
@@ -12,53 +12,69 @@ let salon={
         close:"5:00 p.m",
     },
     phone:"555-555-5555",
-    pets:[
-        {
-            name:"bob",
-            age:50,
-            gender:"male",
-            breed:"dane",
-            service:"grooming",
-            owner:"bob",
-            phone:"999-999-9999"
-        },
-        {
-            name:"john",
-            age:1,
-            gender:"Male",
-            breed:"husky",
-            service:"grooming",
-            owner:"Daniel",
-            phone:"123-123-1234"
-        },
-        {
-
-            name:"jim",
-            age:9,
-            gender:"Male",
-            breed:"mixed",
-            service:"grooming",
-            owner:"gabe",
-            phone:"123-143-1234"
-        }
-
-
-    ]
+    pets:[]
 }
-console.log(salon.pets[0]);//displaying bob
+//console.log(salon.pets[0]);//displaying bob
 
 function displaySalonInfo(){
     let tmp=`
     <p> Welcome to the ${salon.name} located in ${salon.address.city,salon.address.street}</p><p></p>`;
 
     document.getElementById("info").innerHTML=tmp;
+    console.log("displaying");
 }
-displaySalonInfo();
+// //displaySalonInfo();
 
-function displayName(){
-    for(let i=0;i<salon.pets.length;i++){
-        console.log(salon.pets[i].name);
-    }
+// //function displayName(){
+//     for(let i=0;i<salon.pets.length;i++){
+//         console.log(salon.pets[i].name);
+//     }
+// }
+
+// //displayName();
+// //alert(`Registered Pets: ${salon.pets.lenth}`);
+
+function Pet(name,age,gender,breed,service,owner,phone){
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+    this.breed=breed;
+    this.service=service;
+    this.ownersName=owner;
+    this.contactPhone=phone;
+}
+//creating default pets
+    let pet1 = new Pet("Bob",4,"Male","Haircut","Dan",111-111-1112);
+    let pet2 = new Pet("Sparky",1,"Male","Husky","Full","Daniel",520-808-0000);
+    let pet3 = new Pet("Scooby",50,"Male","Dane","Nails Cut","Shaggy",555-111-1111);
+    let pet4 = new Pet("Scrappy",3,"Male","Mixed","Shower","Shaggy",555-111-1111);
+    
+    
+    console.log(pet1,pet2,pet3,pet4);
+
+//register pets
+function register(){
+    console.log("Registering");
+    //get the info from the input*** getElementById .value
+    let petName=document.getElementById("txtName").value;
+    let petAge=document.getElementById("txtAge").value;
+    let petGender=document.getElementById("txtGender").value;
+    let petBreed=document.getElementById("txtBreed").value;
+    let petService=document.getElementById("selService").value;
+    let ownersName=document.getElementById("txtOwner").value;
+    let contactPhone=document.getElementById("txtPhone").value;
+    //creat the object using the constructor
+    let newPet = new Pet(petName,petAge,petGender,petBreed,petService,ownersName,contactPhone)
+    //push the object in on the array
+    salon.pets.push(newPet);
+    //display it in the console
+    console.log(newPet);
+    console.log(salon.pets);
 }
 
-displayName();
+function init(){
+    //hook events, triggered events
+    displaySalonInfo();
+    salon.pets.push(pet1,pet2,pet3,pet4);
+}
+window.onload=init;
